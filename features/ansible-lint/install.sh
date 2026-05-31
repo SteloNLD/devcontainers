d434e8f3fbbab5097c534211b2839bc342ccd552
Sten Lootens <sten@ltns.nl>
2026-05-31 22:26:21 +0000

    Fix ansible-lint to use correct PIPX_HOME from devcontainers-extra ansible feature

--- features/ansible-lint/install.sh
+++ features/ansible-lint/install.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 set -e
 
+export PIPX_HOME=${PIPX_HOME:-"/usr/local/py-utils"}
+export PIPX_BIN_DIR="${PIPX_HOME}/bin"
+export PATH="${PATH}:${PIPX_BIN_DIR}"
+
 pipx inject ansible-core ansible-lint
 
 echo "Installed $(ansible-lint --version)"
